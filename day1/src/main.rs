a52d7a2a50bcde3cbb8c299ae52825fce78c968f
Joshua Davis <pgmjsd@gmail.com>
2022-12-04 10:09:29 -0500

    second commit

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,14 @@
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let mut elves : Vec<i32> = fs::read_to_string("elves.txt").unwrap()
+        .split("\n\n")// Note: won't work on WinDoze, because.
+        .map(|s| {
+            // Parse each item quantity and add them up.
+            s.split("\n").map( |e| { e.parse::<i32>().unwrap() }).sum()
+        }).collect();
+    elves.sort();
+    for e in elves {
+        println!("e={}", e);
+    }
 }
