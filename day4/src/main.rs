e99b5d6d71ad247c51e141713d71d68e1680fc2e
Joshua Davis <pgmjsd@gmail.com>
2022-12-04 21:54:34 -0500

    - break out some functions from the functional pipeline - make some type aliases for better readability

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,31 @@
 use itertools::Itertools;
 use std::fs;
 
-fn range_contains_range(a: (i32, i32), b: (i32, i32)) -> bool {
+// Make some type aliases for readability.
+type Range = (i32, i32);
+
+type Pair = (Range, Range);
+
+// Split the range into two numbers and parse, return a tuple of integers.
+fn parse_range(s : &str) -> Range {
+    s.split("-")
+        .map(|n| {
+            n.parse::<i32>().unwrap()
+        })
+        .collect_tuple()
+        .unwrap() // Since Range is an alias for (i32, i32), this "just works".
+}
+
+// Parse each line into a pair of ranges.
+fn parse_line(line : &str) -> Pair {
+    line
+        .split(",")
+        .map(parse_range)
+        .collect_tuple()
+        .unwrap()   // Since Point is an alias for (Range, Range), this "just works".
+}
+
+fn range_contains_range(a: Range, b: Range) -> bool {
     a.0 <= b.0 && a.1 >= b.1
 }
 
@@ -11,26 +35,16 @@ fn main() {
         .lines()
         .map(|line| {
             // Split each line into pairs of ranges.
-            let (a, b): ((i32, i32), (i32, i32)) = line
-                .split(",")
-                .map(|s| {
-                    s.split("-")
-                        .map(|n| {
-                            // Split the range into two numbers and parse.
-                            n.parse::<i32>().unwrap()
-                        })
-                        .collect_tuple()
-                        .unwrap() // Make the range into a tuple of integers.
-                })
-                .collect_tuple()
-                .unwrap(); // Make the pair into a tuple of ranges.
+            // We can destructure Pair, because it's really just a tuple.
+            let (a, b) = parse_line(line);
             println!("{:?} {:?}", a, b);
+            // Return 1 (to increment the total) if there is an overlap.  Zero if not.
             if range_contains_range(a, b) || range_contains_range(b, a) {
                 1
             } else {
                 0
             }
         })
-        .sum();
+        .sum(); // Add 'em all up.
     println!("total={}", total);
 }
